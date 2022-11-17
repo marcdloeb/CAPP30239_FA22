8290c91463b4b130433e2bb47f6b54bfbe2a9f81
marcdloeb <marcdloeb@gmail.com>
2022-11-16 22:33:23 -0600

    still working on scatter

--- final_project/connected_scatter_test.js
+++ final_project/connected_scatter_test.js
@@ -6,64 +6,78 @@ const svg = d3.select("#chart") //selecting the chart ID from HTML, appending SV
     .append("svg")
     .attr("viewBox", [0, 0, width, height]);
 
-d3.csv('penguins.csv').then(data => {   //getting data
+d3.csv('data/chart_data/nbhood_decade_add_blackper_long.csv').then(data => {   //getting data
   
     console.log(data)
 
-    const islands = ["Torgersen", "Biscoe", "Dream"]
+    // const islands = ["Torgersen", "Biscoe", "Dream"]
 
-    var Color = d3.scaleOrdinal()
-      .domain(islands)
-      .range(d3.schemeSet2);
+    // const dataReady = allGroup.map( function(grpName) { // .map allows to do something for each element of the list
+    //     return {
+    //       name: grpName,
+    //       values: data.map(function(d) {
+    //         return {time: d.time, value: +d[grpName]};
+    //       })
+    //     };
+    //   });
+    //   // I strongly advise to have a look to dataReady with
+    //   console.log(dataReady)
+  
+    // var Color = d3.scaleOrdinal()
+    //   .domain(d3.unique(data, d => d.neighborhoods))
+    //   .range(d3.schemeSet2);
 
     let x = d3.scaleLinear() //setting the x-scale
-        .domain(d3.extent(data, d => d.body_mass_g)).nice() //domain = data set
+        .domain(d3.extent(data, d => d.black_per)).nice() //domain = data set
         .range([margin.left, width - margin.right]);  //range = space data takes on the page
 
-    let y = d3.scaleLinear() //going the same thing on the y-scale
-        .domain(d3.extent(data, d => d.flipper_length_mm)).nice() //d3.extent does max and min
+    let y = d3.scaleSqrt() //going the same thing on the y-scale
+        //.domain(d3.extent(data, d => d.addr_share)).nice() //d3.extent does max and min
+        .domain(d3.extent([0,90])).nice()
         .range([height - margin.bottom, margin.top]);
+
+        console.log(d3.extent(data, d => d.addr_share))
 
     svg.append("g") //creating x-axis
         .attr("transform", `translate(0,${height - margin.bottom})`)
         .attr("class", "x-axis")
-        .call(d3.axisBottom(x).tickFormat(d => (d/1000) + "kg").tickSize(-height + margin.top + margin.bottom))// tick size w/ a negative gives it the grid
+        .call(d3.axisBottom(x).tickFormat(d => (d) + "%").tickSize(-height + margin.top + margin.bottom))// tick size w/ a negative gives it the grid
 
     svg.append("g") //creating y-axis
         .attr("transform", `translate(${margin.left},0)`)
         .attr("class", "y-axis")
-        .call(d3.axisLeft(y).tickSize(-width + margin.left + margin.right))
+        .call(d3.axisLeft(y).tickFormat(d => (d) + "%").tickSize(-width + margin.left + margin.right))
 
     svg.append("g")//appending an HTML element. Know it is HTML because of the quotes
         .attr("fill", "black")
         .selectAll("circle")
         .data(data)
         .join("circle")//joining onto each circle
-        .attr("cx", d => x(d.body_mass_g))//how to position circles on x
-        .attr("cy", d => y(d.flipper_length_mm))//how to position circles on y
+        .attr("cx", d => x(d.black_per))//how to position circles on x
+        .attr("cy", d => y(d.addr_share))//how to position circles on y
         .attr("r", 2)
         .attr("opacity", 0.75); //making semi-transparent
 
-//   const tooltip = d3.select("body").append("div")//go to HTML, select the body tag
-//     .attr("class", "svg-tooltip")
-//     .style("position", "absolute")
-//     .style("visibility", "hidden");
+  const tooltip = d3.select("body").append("div")//go to HTML, select the body tag
+    .attr("class", "svg-tooltip")
+    .style("position", "absolute")
+    .style("visibility", "hidden");
 
-//   d3.selectAll("circle")
-//     .on("mouseover", function(event, d) {
-//       d3.select(this).attr("fill", "red");
-//       tooltip
-//         .style("visibility", "visible")
-//         .html(`Species: ${d.species}<br />Island: ${d.island}<br />Weight: ${d.body_mass_g/1000}kg`);//HTML literals
-//     })
-//     .on("mousemove", function(event) {
-//       tooltip
-//         .style("top", (event.pageY - 10) + "px")
-//         .style("left", (event.pageX + 10) + "px");
-//     })
-//     .on("mouseout", function() {
-//       d3.select(this).attr("fill", "black");
-//       tooltip.style("visibility", "hidden");
-//     })
+  d3.selectAll("circle")
+    .on("mouseover", function(event, d) {
+      d3.select(this).attr("fill", "red");
+      tooltip
+        .style("visibility", "visible")
+        .html(`${d.neighborhood}, ${d.decade}<br />Black Percentage: ${d.black_per}<br />Address Share: ${d.addr_share}`);//HTML literals
+    })
+    .on("mousemove", function(event) {
+      tooltip
+        .style("top", (event.pageY - 10) + "px")
+        .style("left", (event.pageX + 10) + "px");
+    })
+    .on("mouseout", function() {
+      d3.select(this).attr("fill", "black");
+      tooltip.style("visibility", "hidden");
+    })
     
 });
